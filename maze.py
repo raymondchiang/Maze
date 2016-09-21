4d3e6b0f47b70dab5c931ea98c9b54deec85b89e
Anthony Fu <anthonyfu117@hotmail.com>
2016-09-21 20:42:11 +0800

    Simplify

--- maze.py
+++ maze.py
@@ -6,17 +6,18 @@ if os.name == 'nt': # If it's Windows OS
     os.system('chcp 65001') # Change encoding to UTF-8
 #-------------------------------------------
 Size=10  #10*10
-Up=72
-Down=80
-Left=75
-Right=77
+Up=59432
+Down=59440
+Left=59435
+Right=59437
+Directions = [Up, Down, Left, Right]
 #-------------------------------------------
 prt_wall = lambda: termcolor.cprint('  ', 'white', 'on_green', end='')
 prt_road = lambda: termcolor.cprint('  ', 'white', 'on_white', end='')
 prt_door = lambda: termcolor.cprint('  ', 'white', 'on_red', end='')
 prt_border = lambda: termcolor.cprint('  ', 'white', 'on_yellow', end='')
 prt_player = lambda: termcolor.cprint('  ', 'white', 'on_blue', end='')
-blocks = [prt_road, prt_wall, prt_door,prt_player]
+blocks = [prt_road, prt_wall, prt_door, prt_player, prt_border]
 #--------------------------------------------------------------------
 road=[
 [3,0,1,1,1,1,1,1,0,1],
@@ -28,112 +29,123 @@ road=[
 [1,0,0,0,0,0,0,0,0,0],
 [0,0,1,0,1,1,0,1,1,0],
 [0,1,1,0,1,1,0,1,1,0],
-[0,1,0,0,0,0,0,0,1,2]]
+[0,1,0,0,0,0,0,2,1,0]]
 current=[0,0] # Coordinate
+step=0 # How many steps you use in the game
 for row in range(Size): # Set first site
     for column in range(Size):
         if road[row][column]==3:
             current=[row,column]
+            road[row][column]=0
+            break
 #--------------------------------------------------------------------
-def Global_Map(road):
-    for row in range(len(road)):
-        for column in range(len(road)):
-            blocks[road[row][column]]()
-        print()
-    print()
-def Show_Maze(current,road,step,zoom=1):
-    print('─────────')
-    print(current)
-    x = current[0]-2
-    y = current[1]-2
-    print(x,y)
-    for row in range(x, x+5):
+def GetBlock(x, y):
+    global current, road
+
+    if x == current[0] and y == current[1]:
+        return 3
+    elif x<0 or y<0 or x>=Size or y>=Size:
+        return 4
+    else:
+        return road[x][y]
+
+def Show_Maze(start=None, end=None, zoom=1):
+    start = start or [0,0]
+    end = end or [Size-1,Size-1]
+    for row in range(start[0],end[0]):
         for _ in range(zoom):
-            for col in range(y, y+5):
+            for col in range(start[1], end[1]):
                 for _ in range(zoom):
-                    if col<0 or row<0 or row>Size-1 or col>Size-1:
-                        prt_border()
-                    else:
-                        if road[row][col]==2:
-                            prt_door()
-                        elif road[row][col]==1:
-                            prt_wall()
-                        elif road[row][col]==0:
-                            prt_road()
-                        else:
-                            prt_player()
+                    blocks[GetBlock(row,col)]()
             print()
-    print('─────────\nStep',step[0])
-def Move(road,current,loc,step):
-    if ord(loc)==Up:
-        if current[0]==0 or road[current[0]-1][current[1]]==1:
-            return True
-        else:
-            road[current[0]-1][current[1]],road[current[0]][current[1]]=road[current[0]][current[1]],road[current[0]-1][current[1]]
-            current[0]-=1
-            step[0]+=1
-    elif ord(loc)==Left:
-        if current[1]==0 or road[current[0]][current[1]-1]==1:
-            return True
-        else:
-            road[current[0]][current[1]-1],road[current[0]][current[1]]=road[current[0]][current[1]],road[current[0]][current[1]-1]
-            current[1]-=1
-            step[0]+=1
-    elif ord(loc)==Down:
-        if current[0]==9 or road[current[0]+1][current[1]]==1:
-            return True
-        else:
-            road[current[0]+1][current[1]],road[current[0]][current[1]]=road[current[0]][current[1]],road[current[0]+1][current[1]]
-            current[0]+=1
-            step[0]+=1
-    elif ord(loc)==Right:
-        if current[1]==9 or road[current[0]][current[1]+1]==1:
-            return True
-        else:
-            road[current[0]][current[1]+1],road[current[0]][current[1]]=road[current[0]][current[1]],road[current[0]][current[1]+1]
-            current[1]+=1
-            step[0]+=1
+
+def Move(direction):
+    global current, step
+    x = current[0]
+    y = current[1]
+    if direction==Up:
+        x -= 1
+    elif direction==Down:
+        x += 1
+    elif direction==Left:
+        y -= 1
+    elif direction==Right:
+        y += 1
+    else:
+        return False
+
+    block = GetBlock(x,y)
+    if block == 4 or block == 1:
+        return False
+    else:
+        current = [x,y]
+        step += 1
+        return True
+
+def get_unicode():
+    code = 0
+    while True:
+        chcode = ord(msvcrt.getch())
+        code *= 265
+        code += chcode
+        if chcode != 224:
+            return code
+
+def clear():
+    os.system('cls')
+
 def introduce():
+    clear()
     print('──────────────────────────')
-    print('**Keyword**\n   h : help\n   q : quit\n   c : cheat')
+    print('**Keyword**\n   h : help\n   q : quit')
     print('──────────────────────────\n')
     print('**How to Play**')
     print('   Use arrow key to move !!!\n')
     print('──────────────────────────\n')
-    print('Press Anywhere To Continue~~~')
-    msvcrt.getch()
+    print('Press Enter To Continue~~~')
+
+def help():
+    introduce()
+
+def start():
+    introduce()
+    key = input()
+    if key == 'q':
+        return
+    game()
+
+
+def game():
+    global current, step
+    moved = True
+    zoom = 2
+    while True:
+        if moved:
+            clear()
+            x,y = current
+            print(' Step:',step,'Coordinare:', x,',',y)
+            Show_Maze(start=[x-2,y-2],end=[x+3,y+3],zoom=zoom)
+            print()
+
+        moved = False
+        code = get_unicode()
+
+        if code == ord('q'):
+            print('Bye Bye~~')
+            return
+        elif code == ord('c'):
+            print('Fuck you bitch you cheat!')
+            Show_Maze()
+        elif code == ord('z'):
+            zoom += 1
+            if zoom >= 4:
+                zoom = 1
+            moved = True
+        elif code in Directions:
+            moved = Move(code)
+
+
 #------------------------------------------------------------
-cheat=False
-need_help=False
-step=[0,] # How many steps you use in the game
-is_clean=True
-print("\nWelcome Raymond's Maze!! ")
-introduce()
-while True:
-    if is_clean:
-        is_clean=False
-    else:
-        os.system('cls') # Refresh screen
-        Show_Maze(current,road,step,3)
-    if cheat==True:
-        cheat=False
-        Global_Map(road)
-        print('Coordinare :',current,end=' \n\n')
-    if need_help==True:
-        need_help=False
-        introduce()
-    Keyword = msvcrt.getch()
-    if ord(Keyword)==99:#c-->cheat
-        cheat=True
-        continue
-    if ord(Keyword)==113:#q-->quit
-        print('Bye Bye~~')
-        break
-    if ord(Keyword)==104:#h-->help
-        need_help=True
-        continue
-    loc = msvcrt.getch()
-    is_clean=Move(road,current,loc,step)
-    if current==[Size,Size]:
-        print('Congratulations !! ')
-        break
+if __name__ == '__main__':
+    print("\nWelcome Raymond's Maze!! ")
+    start()
