b56506acf0acf2f08e0e0c2eb6afa76e0bb1efb7
raymondchiang <chiangnarry@gmail.com>
2016-09-21 22:39:18 +0800

    Implement Move for level.py

--- level.py
+++ level.py
@@ -1,3 +1,5 @@
+from constants import *
+
 class Level:
     # public Level()
     def __init__(self, maze, size):
@@ -5,8 +7,9 @@ class Level:
         self.size = size # [x,y]
         self.current = [0,0]
         self.step = 0
+        self.gameover = False
 
-    def get_block(self, x, y):
+    def GetBlock(self, x, y):
         if x == self.current[0] and y == self.current[1]:
             return 3
         elif x<0 or y<0 or x>=self.size[0] or y>=self.size[1]:
@@ -14,5 +17,26 @@ class Level:
         else:
             return self.maze[x][y]
 
-    def move(self, direction):
-        pass
+    def Move(self, direction):
+        x = self.current[0]
+        y = self.current[1]
+        if direction==UP:
+            x -= 1
+        elif direction==DOWN:
+            x += 1
+        elif direction==LEFT:
+            y -= 1
+        elif direction==RIGHT:
+            y += 1
+        else:
+            return False
+
+        block = self.GetBlock(x,y)
+        if block == 4 or block == 1:
+            return False
+        else:
+            if block == 2:
+                self.gameover = True
+            self.current = [x,y]
+            self.step += 1
+            return True
